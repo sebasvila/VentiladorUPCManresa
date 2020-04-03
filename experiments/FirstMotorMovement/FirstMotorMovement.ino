3ffcfca32d0afcb6abc764b3270e5ed87823c818
PerePala <PerePala@users.noreply.github.com>
2020-04-03 12:03:49 +0200

    First Motor Movement

---
+++ experiments/FirstMotorMovement/FirstMotorMovement.ino
@@ -0,0 +1,48 @@
+/*
+  This sketch assumes a single
+  DRV8825 DRIVER in Y slot of the CNC Board
+*/
+
+# define EN    8
+# define DIR   6
+# define STP   3
+
+# define stepsPerTurn 200
+# define step_us  600
+# define maxTurns 10
+
+
+unsigned long time0;
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(EN , OUTPUT);
+  pinMode(DIR, OUTPUT);
+  pinMode(STP, OUTPUT);
+  
+  digitalWrite(EN , LOW );
+  digitalWrite(DIR, LOW);
+
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  time0 = millis();
+  for (int i = 0; i < stepsPerTurn; i++) {
+    
+    digitalWrite(STP, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delayMicroseconds(step_us);
+    
+    digitalWrite(STP, LOW);
+    digitalWrite(LED_BUILTIN, LOW); 
+    delayMicroseconds(step_us);
+    
+  }
+  Serial.println(millis()-time0);
+  
+  delay(1000);
+}
