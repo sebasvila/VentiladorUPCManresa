b767e568d2b9432fb37b3a33b571305384fc90ce
PerePala <PerePala@users.noreply.github.com>
2020-04-09 18:11:05 +0200

    added mqtt experiments

---
+++ ESP32experiments/boot.py
@@ -0,0 +1,40 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+import time
+from umqttsimple import MQTTClient
+import ubinascii
+import machine
+import micropython
+import network
+import esp
+esp.osdebug(None)
+import gc
+gc.collect()
+
+from myconfig import *
+#  ssid = 'REPLACE_WITH_YOUR_SSID'
+#  password = 'REPLACE_WITH_YOUR_PASSWORD'
+#  mqtt_server = 'REPLACE_WITH_YOUR_MQTT_BROKER_IP'
+
+
+client_id = ubinascii.hexlify(machine.unique_id())
+topic_sub = b'notification'
+topic_pub = b'test'
+
+last_message = 0
+message_interval = 1
+counter = 0
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+print('Hi!')
+
---
+++ ESP32experiments/main.py
@@ -0,0 +1,35 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+def connect():
+  global client_id, mqtt_server
+  client = MQTTClient(client_id, mqtt_server)
+  client.connect()
+  print('Connected to %s MQTT broker' % (mqtt_server))
+  return client
+
+def restart_and_reconnect():
+  print('Failed to connect to MQTT broker. Reconnecting...')
+  time.sleep(10)
+  machine.reset()
+
+pot=machine.ADC(machine.Pin(33))
+pot.width(machine.ADC.WIDTH_10BIT)
+pot.atten(machine.ADC.ATTN_11DB)
+
+try:
+  client = connect()
+
+except OSError as e:
+  restart_and_reconnect()
+
+while True:
+  try:
+    client.check_msg()
+    if (time.time() - last_message) > message_interval:
+      msg = b'pot > %d' % pot.read()
+      client.publish(topic_pub, msg)
+      last_message = time.time()
+      counter += 1
+  except OSError as e:
+    restart_and_reconnect()
+
