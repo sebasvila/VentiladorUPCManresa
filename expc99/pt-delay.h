d4077a5e257a935c185111e619741df393d88c67
Sebas Vila <sebas.vima@gmail.com>
2020-04-20 16:03:31 +0200

    Add a new delay macro with function semantics.

--- expc99/pt-delay.h
+++ expc99/pt-delay.h
@@ -26,6 +26,30 @@
   } while(0)
 
 
+/*
+ * The same meaning that last macro but with a local copy of parameter.
+ *
+ * Note that first macro, although similar to a function is not a
+ * function. Thus, if `time` parameter changes during execution, new
+ * value will be used in next check after thread wakeup. The
+ * `PT_DELAY_F` has real function semantics and, once 'called', `time`
+ * will be bound at call time.
+ */
+#define PT_DELAY_F(pt, time)			\
+  do {						\
+    static uint16_t from, p;			\
+						\
+    p = time;					\
+    from = ticker_get();			\
+    LC_SET((pt)->lc);				\
+    if( (ticker_get() - from <= (time)) ) {	\
+      return PT_WAITING;			\
+    }						\
+  } while(0)
+
+
+
+
 #endif /* _PT_DELAY_H_ */
 
 
