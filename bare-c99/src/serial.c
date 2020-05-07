fb2f3960c377119da447ba6ba5cd0f366b6d8d34
Sebastià Vila <sebas@napbuf>
2020-05-07 14:07:06 +0200

    Refactorized: tx and rx can be included/excluded at compilation time; function names rationalized; adopted open/close api pattern.

--- bare-c99/src/serial.c
+++ bare-c99/src/serial.c
@@ -7,31 +7,24 @@
 
 #define NUM (F_CPU/16)
 #define BAUDRATE_L(x) UINT8_C((NUM/x-1)      & 0xff)
-#define BAUDRATE_H(x) UINT8_C((NUM/x-1) >> 8 & 0xf)
+#define BAUDRATE_H(x) UINT8_C((NUM/x-1) >> 8 & 0x0f)
 
 
 /*
  * Input and output queues
  */
-static queue_t inq;   // For received data  
-static queue_t outq;  // For data to be sent
-
+#ifdef _SER_RX_
+static volatile queue_t inq;   // For received data
+#endif
+#ifdef _SER_TX_
+static volatile queue_t outq;  // For data to be sent
+#endif
 
 /*
  * ISR's
  */
-ISR(USART_UDRE_vect) {
-  // USART ready to transmit a byte
-  if (queue_is_empty(&outq)) {
-    // No data to send => disable UDRE interrupt (19.6.3)
-    UCSR0B &= ~_BV(UDRIE0);
-  } else {
-    UDR0 = queue_front(&outq);
-    queue_dequeue(&outq);
-  }
-}
 
-
+#ifdef _SER_RX_
 ISR(USART_RX_vect) {
   // USART byte received: enqueue or discard
   if (queue_is_full(&inq)) {
@@ -41,17 +34,103 @@ ISR(USART_RX_vect) {
     queue_enqueue(&inq, UDR0);
   }
 }
+#endif
+
+#ifdef _SER_TX_
+ISR(USART_UDRE_vect) {
+  // USART ready to transmit a byte
+  if (queue_is_empty(&outq)) {
+    // No data to send => disable UDRE interrupt (¶19.6.3)
+    UCSR0B &= ~_BV(UDRIE0);
+  } else {
+    UDR0 = queue_front(&outq);
+    queue_dequeue(&outq);
+  }
+}
+#endif
 
 
-void serial_init(void) {
+
+#ifdef _SER_RX_
+bool serial_can_read(void) {
+  // test whether there is something to read
+  return !queue_is_empty(&inq);
+}
+
+
+uint8_t serial_read(void) {
+  while (queue_is_empty(&inq)); // better put to sleep
+  uint8_t r = queue_front(&inq);
+  queue_dequeue(&inq);
+  return r;
+}
+#endif
+
+
+
+#ifdef _SER_TX_
+bool serial_can_write(uint8_t c) {
+  return !queue_is_full(&outq);
+}
+
+
+void serial_write(uint8_t c) {
+  while (queue_is_full(&outq)); // better put to sleep
+  queue_enqueue(&outq, c);
+  // Once there is data, it can transmit => activate interrupts
+  UCSR0B |= _BV(UDRIE0);
+}
+#endif
+
+
+void serial_open(void) {
+  #ifdef _SER_RX
+  // Enable reception
+  UCSR0B |= _BV(RXEN0);
+  // Enable rx interrupts
+  UCSR0B |= _BV(RXCIE0);
+  #endif
+  #ifdef _SER_TX
+  // Enable transmision
+  UCSR0B |=  _BV(TXEN0);
+  #endif
+
+}
+
+
+void serial_close(void) {
+  #ifdef _SER_TX_
+  /* wait last byte sent 
+   * waits that tx-isr disables "data register empty" interrupt:
+   * this means that tx queue emptied and last char was sent 
+   */
+  while (UCSR0B & _BV(UDRIE0));
+  /* disable transmision*/
+  UCSR0B &= ~_BV(TXEN0);  
+  #endif
+  #ifdef _SER_RX_
+  /* disable receive interrupts */
+  UCSR0B &= ~_BV(RXCIE0);  
+  /* disable reception */
+  UCSR0B &= ~_BV(RXEN0);
+  #endif
+}
+
+
+
+void serial_setup(void) {
   // Initialize the queues
+  #ifdef _SER_RX_
   queue_empty(&inq);
+  #endif
+  #ifdef _SER_TX_
   queue_empty(&outq);
+  #endif
 
-  // Initialize the UART0
-  // According to 19.5 we must wait last transmision finishes
-  // and all data received... ignoring it
-  // According to 19.5 interrupts must be disabled and restored at end
+  // Initialize the UART0 According to ¶19.5 we must wait last
+  // transmision finishes and all data received: ignoring it.
+  // According to ¶19.5 interrupts must be disabled and restored at
+  // end: assume setup is done with interrupts disabled always.
   UBRR0H = BAUDRATE_H(9600);
   UBRR0L = BAUDRATE_L(9600);
   // set normal baud rate 
@@ -61,35 +140,5 @@ void serial_init(void) {
     ~_BV(UMSEL01) & ~_BV(UMSEL00) &   // asincronous mode
     ~_BV(UPM01)   & ~_BV(UPM00)   &   // no parity
     ~_BV(USBS0)   ;                   // 1 stop bit
-  // Enable transmision and reception
-  UCSR0B = _BV(RXEN0) | _BV(TXEN0);
-  // Enable interrupts only for receiving
-  UCSR0B |= _BV(RXCIE0);
-}
-
-
-
-uint8_t serial_get(void) {
-  while (queue_is_empty(&inq)); // better put to sleep
-  uint8_t r = queue_front(&inq);
-  queue_dequeue(&inq);
-  return r;
-}
-
-
-bool serial_can_read(void) {
-  // test whether there is something to read
-  return !queue_is_empty(&inq);
-}
-
-
-/*
- * Enqueue data to be sent. Blocks if queue is full.
- */
-void serial_put(uint8_t c) {
-  while (queue_is_full(&outq)); // better put to sleep
-  queue_enqueue(&outq, c);
-  // Once there is data, it can transmit => activate interrupts
-  UCSR0B |= _BV(UDRIE0);
 }
 
--- bare-c99/src/serial.h
+++ bare-c99/src/serial.h
@@ -4,9 +4,34 @@
 #include <inttypes.h>
 #include <stdbool.h>
 
-void serial_init(void);
-uint8_t  serial_get(void);
-void serial_put(uint8_t c);
+/* macros SER_ONLY_TX and SER_ONLY_RX allow to choose only
+ * half serial communication during compile time.
+ * Default: both tx and rx
+ */
+#if defined(SER_ONLY_TX) || !defined(SER_ONLY_RX))
+#define _SER_TX_
+#endif
+
+#if defined(SER_ONLY_RX) || !defined(SER_ONLY_TX))
+#define _SER_RX_
+#endif
+
+
+
+
+void serial_setup(void);
+
+void serial_open(void);
+void serial_close(void);
+
+#ifdef _SER_RX_
 bool serial_can_read(void);
+uint8_t  serial_read(void);
+#endif
+
+#ifdef _SER_TX_
+bool serial_can_write(void);
+void serial_write(uint8_t c);
+#endif
 
 #endif
