92c54222287dfa5bd1f1627e75cb2140016410c4
Pau Lafoz <pau@lacodornella.cat>
2021-03-02 11:27:17 +0100

    UI added to main

--- vcontrol/src/main.c
+++ vcontrol/src/main.c
@@ -6,25 +6,25 @@
 
 int main(void) {
   /* context dels threads */
-  struct pt vaction_ctxt, display_ctx, buttons_ctx, encoder_ctx;
+  struct pt vaction_ctxt, display_ctxt, buttons_ctxt, encoder_ctxt;
 
   /* init modules */
+  sei();
   UI_setup();
   vaction_setup();
-  sei();
 
 
   /* init thread contexts */
-  PT_INIT(&buttons_ctx);
-  PT_INIT(&encoder_ctx);
+  PT_INIT(&buttons_ctxt);
+  PT_INIT(&encoder_ctxt);
+  PT_INIT(&display_ctxt);
   PT_INIT(&vaction_ctxt);
-  PT_INIT(&display_ctx);
   
   /* do schedule */
   for(;;) {
-    (void)PT_SCHEDULE(buttons_thread(&buttons_ctx));
-    (void)PT_SCHEDULE(encoder_thread(&encoder_ctx));
+    (void)PT_SCHEDULE(buttons_thread(&buttons_ctxt));
+    (void)PT_SCHEDULE(encoder_thread(&encoder_ctxt));
+    (void)PT_SCHEDULE(display_thread(&display_ctxt));
     (void)PT_SCHEDULE(vaction_thread(&vaction_ctxt));
-    (void)PT_SCHEDULE(display_thread(&display_ctx));
   }
 }
